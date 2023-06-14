a017ccc2662d5cdd108a33b6093596375a021eee
Karan Kumar <karankumark19k@gmail.com>
2023-06-14 15:43:54 +0530

    Basic DI setup

---
+++ app/src/main/java/com/example/jetlibrary/JetLibraryApplication.kt
@@ -0,0 +1,8 @@
+package com.example.jetlibrary
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JetLibraryApplication: Application() {
+}
--- app/src/main/java/com/example/jetlibrary/MainActivity.kt
+++ app/src/main/java/com/example/jetlibrary/MainActivity.kt
@@ -1,46 +1,59 @@
 package com.example.jetlibrary
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetlibrary.ui.theme.JetLibraryTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetLibraryTheme {
+
+
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+
+                user["firstName"] = "Karan"
+                user["lastName"] = "Kumar"
+
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener {
+                        Log.d("onCreate", "Success")
+                    }.addOnFailureListener {
+                        Log.d("onCreate", "Failure ")
+                    }
+
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetLibraryTheme {
-        Greeting("Android")
+
     }
 }
